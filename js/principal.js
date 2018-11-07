8df368f621e6bb21f507674bfb5e96f295fb0af5
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-06 23:46:04 -0200

    (02) - Condições e validações

--- js/principal.js
+++ js/principal.js
@@ -4,13 +4,27 @@ titulo.textContent = 'Smart Nutri';
 var primeiroPaciente = document.querySelector('#primeiro-paciente');
 var tdPeso = primeiroPaciente.querySelector('.info-peso');
 var tdAltura = primeiroPaciente.querySelector('.info-altura');
+var tdImc = primeiroPaciente.querySelector('.info-imc');
 
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
-console.log(peso);
-console.log(altura);
 
-var valorImc = peso / (altura * altura);
-console.log('IMC: '+ valorImc);
-var tdImc = primeiroPaciente.querySelector('.info-imc');
-tdImc.textContent = valorImc;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdPeso.textContent = 'Peso inválido';
+}
+if(altura <= 0 || altura >= 3.00) {
+    alturaEhValida = false;
+    tdAltura.textContent = 'Altura inválida';
+}
+
+if(pesoEhValido && alturaEhValida) {
+    var valorImc = peso / (altura * altura);
+    console.log('IMC: '+ valorImc);
+    tdImc.textContent = valorImc;
+} else {
+    tdImc.textContent = 'Peso e/ou altura inválidos!';
+}
