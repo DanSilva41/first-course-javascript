939bef30fc7ca9785f561f2853c6240afe5f932f
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-08 21:51:00 -0200

    (03) Replicando cálculo e validações com iteração em JS

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,36 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Smart Nutri';
 
-var primeiroPaciente = document.querySelector('#primeiro-paciente');
-var tdPeso = primeiroPaciente.querySelector('.info-peso');
-var tdAltura = primeiroPaciente.querySelector('.info-altura');
-var tdImc = primeiroPaciente.querySelector('.info-imc');
+var pacientes = document.querySelectorAll('.paciente');
+console.log(pacientes);
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+pacientes.forEach(calcularImc);
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+function calcularImc(paciente) {
+    var tdPeso = paciente.querySelector('.info-peso');
+    var tdAltura = paciente.querySelector('.info-altura');
+    var tdImc = paciente.querySelector('.info-imc');
 
-if(peso <= 0 || peso >= 1000) {
-    pesoEhValido = false;
-    tdPeso.textContent = 'Peso inválido';
-}
-if(altura <= 0 || altura >= 3.00) {
-    alturaEhValida = false;
-    tdAltura.textContent = 'Altura inválida';
-}
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-if(pesoEhValido && alturaEhValida) {
-    var valorImc = peso / (altura * altura);
-    console.log('IMC: '+ valorImc);
-    tdImc.textContent = valorImc;
-} else {
-    tdImc.textContent = 'Peso e/ou altura inválidos!';
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        tdPeso.textContent = 'Peso inválido';
+    }
+    if(altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdAltura.textContent = 'Altura inválida';
+    }
+
+    if(pesoEhValido && alturaEhValida) {
+        var valorImc = peso / (altura * altura);
+        console.log('IMC: '+ valorImc);
+        tdImc.textContent = valorImc;
+    } else {
+        tdImc.textContent = 'Peso e/ou altura inválidos!';
+    }
 }
