56c96f363610a0076cb62c34eae1e61cdca9ff36
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-09 23:10:03 -0200

    (05) Organizando código JS

---
+++ js/calcula-imc.js
@@ -0,0 +1,36 @@
+var pacientes = document.querySelectorAll('.paciente');
+console.log(pacientes);
+
+for(var i = 0; i < pacientes.length; i++) {
+    calcularImc(pacientes[i]);
+}
+
+function calcularImc(paciente) {
+    var tdPeso = paciente.querySelector('.info-peso');
+    var tdAltura = paciente.querySelector('.info-altura');
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        tdPeso.textContent = 'Peso inválido';
+    }
+    if(altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdAltura.textContent = 'Altura inválida';
+    }
+
+    if(pesoEhValido && alturaEhValida) {
+        var valorImc = peso / (altura * altura);
+        console.log('IMC: '+ valorImc);
+        tdImc.textContent = valorImc.toFixed(2);
+    } else {
+        tdImc.textContent = 'Peso e/ou altura inválidos!';
+        paciente.classList.add("paciente-invalido");
+    }
+}
