9331a6b9440731ef03896ebb993ab01b2c370c21
Danilo da Silva Pereira <dansilva41@hotmail.com>
2018-11-09 23:00:58 -0200

    (04) Recebendo elementos do form e adicionando na tabela

--- js/principal.js
+++ js/principal.js
@@ -52,7 +52,32 @@ var btnAdicionarPaciente = document.querySelector("#adicionar-paciente");
 btnAdicionarPaciente.addEventListener("click", function(event) {
     event.preventDefault();
     console.log("Adicionando paciente....");
-    adicionarDiv();
+    
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabelaPacientes = document.querySelector("#tabela-pacientes");
+    tabelaPacientes.appendChild(pacienteTr);
+
 });
 
 function adicionarDiv() {
